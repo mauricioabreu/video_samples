4eb400b2f29e35ae455c89e1d1c2298390986081
Maurício Antunes <mauricio.abreua@gmail.com>
2019-06-08 21:59:27 -0300

    Save thumbs on path passed by argument

--- main.go
+++ main.go
@@ -17,12 +17,12 @@ import (
 func main() {
 	thumbsPath := "thumbnails"
 	streamingURL := "http://127.0.0.1:8080/play/hls/bunny/index.m3u8"
-	generateThumb(streamingURL)
+	generateThumb(streamingURL, thumbsPath)
 	collectThumbs(thumbsPath)
 }
 
-func generateThumb(streamingURL string) {
-	args := []string{"-live_start_index", "-1", "-f", "hls", "-i", fmt.Sprintf("%s", streamingURL), "-vf", "fps=1,scale=-1:169", "-vsync", "vfr", "-q:v", "5", "-threads", "1", "thumbnails/%09d.jpg"}
+func generateThumb(streamingURL string, path string) {
+	args := []string{"-live_start_index", "-1", "-f", "hls", "-i", fmt.Sprintf("%s", streamingURL), "-vf", "fps=1,scale=-1:169", "-vsync", "vfr", "-q:v", "5", "-threads", "1", fmt.Sprintf("%s/%%09d.jpg", path)}
 	cmd := exec.Command("ffmpeg", args...)
 	err := cmd.Start()
 	if err != nil {
