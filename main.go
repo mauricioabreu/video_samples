8c60637ad054d1169f97c209d2748a7a92293b60
Maurício Antunes <mauricio.abreua@gmail.com>
2019-06-09 22:16:29 -0300

    Save thumbs on their reserved namespaces

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"time"
 
@@ -14,21 +15,54 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+type stream struct {
+	name string
+	URL  string
+}
+
 func main() {
 	thumbsPath := "thumbnails"
-	streamingURL := "http://127.0.0.1:8080/play/hls/bunny/index.m3u8"
-	generateThumb(streamingURL, thumbsPath)
+	streams := []stream{
+		stream{
+			name: "big_buck_bunny",
+			URL:  "http://127.0.0.1:8080/play/hls/bunny/index.m3u8",
+		},
+	}
+	for _, s := range streams {
+		log.Printf("Generating thumbs for %s with URL %s", s.name, s.URL)
+		generateThumb(s.URL, s.name, thumbsPath)
+	}
 	collectThumbs(thumbsPath)
 }
 
-func generateThumb(streamingURL string, path string) {
-	args := []string{"-live_start_index", "-1", "-f", "hls", "-i", fmt.Sprintf("%s", streamingURL), "-vf", "fps=1,scale=-1:169", "-vsync", "vfr", "-q:v", "5", "-threads", "1", fmt.Sprintf("%s/%%09d.jpg", path)}
+func createDir(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, 0755)
+	}
+}
+
+func generateThumb(streamingURL string, streamName string, path string) {
+	createDir(filepath.Join(path, streamName))
+	args := []string{"-live_start_index", "-1", "-f", "hls", "-i", fmt.Sprintf("%s", streamingURL), "-vf", "fps=1,scale=-1:169", "-vsync", "vfr", "-q:v", "5", "-threads", "1", fmt.Sprintf("%s/%s/%%09d.jpg", path, streamName)}
 	cmd := exec.Command("ffmpeg", args...)
 	err := cmd.Start()
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Generating thumbnail for %s\n", streamingURL)
+}
+
+func getSubDirs(sourcePath string) []string {
+	var paths []string
+	filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	return paths
 }
 
 func collectThumbs(path string) {
@@ -48,6 +82,11 @@ func collectThumbs(path string) {
 	if err := watcher.Add(path); err != nil {
 		log.Fatal(err)
 	}
+	for _, d := range getSubDirs(path) {
+		if err := watcher.Add(d); err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	done := make(chan bool)
 	go func() {
@@ -64,11 +103,12 @@ func collectThumbs(path string) {
 					log.Fatalf("Could not read path metadata for %s: %s", event.Name, err)
 				}
 				timestamp := pathInfo.ModTime().UTC().Unix()
-				if err := saveThumb(client, "big_buck_bunny", timestamp, data); err != nil {
-					log.Printf("Could not save thumbs for %s: %s", "big_buck_bunny", err)
+				streamName := getStreamName(event.Name)
+				if err := saveThumb(client, streamName, timestamp, data); err != nil {
+					log.Printf("Could not save thumbs for %s: %s", streamName, err)
 					continue
 				}
-				log.Printf("Saved thumb for %s", "big_buck_bunny")
+				log.Printf("Saved thumb for %s", streamName)
 				if err := os.Remove(event.Name); err != nil {
 					log.Printf("Could not remove thumb file %s: %s", event.Name, err)
 				}
@@ -83,6 +123,10 @@ func collectThumbs(path string) {
 	}
 }
 
+func getStreamName(filename string) string {
+	return filepath.Base(filepath.Dir(filename))
+}
+
 func saveThumb(c *redis.Client, key string, timestamp int64, blob []byte) error {
 	id, err := uuid.NewV4()
 	if err != nil {
@@ -94,7 +138,7 @@ func saveThumb(c *redis.Client, key string, timestamp int64, blob []byte) error 
 		return err
 	}
 
-	if err = c.Set(id.String(), blob, time.Duration(60)*time.Second).Err(); err != nil {
+	if err = c.Set(fmt.Sprintf("thumbs/blob/%s", id.String()), blob, time.Duration(60)*time.Second).Err(); err != nil {
 		return err
 	}
 
