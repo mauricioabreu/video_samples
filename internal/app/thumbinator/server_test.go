b1d9b848529a0d178b1994bcb2244383c7f3b4d6
Maurício Antunes <mauricio.abreua@gmail.com>
2019-06-22 09:04:35 -0300

    Test query thumb by timestamp

--- internal/app/thumbinator/server_test.go
+++ internal/app/thumbinator/server_test.go
@@ -13,7 +13,7 @@ func (ds dummyStore) GetThumb(streamName string) string {
 }
 
 func (ds dummyStore) GetThumbByTimestamp(streamName string, timestamp int64) string {
-	return "thumb_blob_here"
+	return "thumb_blob_by_timestamp_here"
 }
 
 func (ds dummyStore) SaveThumb(stream Stream, timestamp int64, blob []byte) error {
@@ -38,6 +38,31 @@ func TestRetrieveSnapshot(t *testing.T) {
 
 	if body := rr.Body; body.String() != "thumb_blob_here" {
 		t.Errorf("Returnted wrong body: got %v wanted %v", body.String(), "thumb_blob_here")
+	}
+
+	if headers := rr.Header(); headers.Get("Content-Type") != "image/jpeg" {
+		t.Errorf("Returned wrong header: got %v wanted image/jpeg", headers.Get("Content-Type"))
+	}
+}
+
+func TestRetrieveSnapshotByTimestamp(t *testing.T) {
+	server := Server{store: dummyStore{}}
+	req, err := http.NewRequest("GET", "/?timestamp=1561204928", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(server.showSnapshot)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Returned wrong status code: got %v wanted %v",
+			status, http.StatusOK)
+	}
+
+	if body := rr.Body; body.String() != "thumb_blob_by_timestamp_here" {
+		t.Errorf("Returnted wrong body: got %v wanted %v", body.String(), "thumb_blob_by_timestamp_here")
 	}
 
 	if headers := rr.Header(); headers.Get("Content-Type") != "image/jpeg" {
