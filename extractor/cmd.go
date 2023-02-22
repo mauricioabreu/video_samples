76e94f51d244c82b763e499f1943f0a0a14fe4a0
Maurício Antunes <mauricio.abreua@gmail.com>
2023-02-22 09:42:32 -0300

    feat: Log ffmpeg command

--- extractor/cmd.go
+++ extractor/cmd.go
@@ -1,8 +1,12 @@
 package extractor
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os/exec"
+
+	"github.com/rs/zerolog/log"
 )
 
 type Command struct {
@@ -12,13 +16,29 @@ type Command struct {
 
 func RunCmd(c Command) error {
 	cmd := exec.Command(c.executable, c.args...) //#nosec G204
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("failed to create a pipe from stderr: %w", err)
+	}
+
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("failed to start cmd: %w", err)
 	}
 
-	if err := cmd.Start(); err != nil {
+	logCmd(stderr)
+
+	if err := cmd.Wait(); err != nil {
 		return fmt.Errorf("failed to wait cmd: %w", err)
 	}
 
-	return cmd.Wait()
+	return nil
+}
+
+// logCmd capture the buffer and log each line of it
+func logCmd(rc io.ReadCloser) {
+	scanner := bufio.NewScanner(rc)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		log.Info().Msg(scanner.Text())
+	}
 }
