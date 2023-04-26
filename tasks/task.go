c95065b015f068cfa025e29d026df19b6fc01010
Maurício Antunes <mauricio.abreua@gmail.com>
2023-04-26 09:10:05 -0300

    fix: Format error with %v

--- tasks/task.go
+++ tasks/task.go
@@ -25,11 +25,11 @@ func NewExtractThumbsTask(to extractor.ThumbOptions) (*asynq.Task, error) {
 func HandleThumbsExtractTask(ctx context.Context, t *asynq.Task) error {
 	var opts extractor.ThumbOptions
 	if err := json.Unmarshal(t.Payload(), &opts); err != nil {
-		return fmt.Errorf("failed to process payload: %w %w", err, asynq.SkipRetry)
+		return fmt.Errorf("failed to process payload: %v %w", err, asynq.SkipRetry)
 	}
 	log.Info().Msgf("Extracting thumbs from video URL: %s", opts.Input)
 	if err := extractor.ExtractThumbs("colors", opts, extractor.RunCmd); err != nil {
-		return fmt.Errorf("failed to run the extractor: %w %w", err, asynq.SkipRetry)
+		return fmt.Errorf("failed to run the extractor: %v %w", err, asynq.SkipRetry)
 	}
 	return nil
 }
